e6fa88bca073757f45d46058161b37b63792d9e4
cuongtadonald <cuongtadonald@gmail.com>
2021-04-24 07:30:38 +0700

    Upload Project second time

---
+++ ShopSnacktvc/shopsnackDC/shopsnackDC/Models/BUS/shopsnackDCBUS.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using shopsnackDCConnection;
+
+namespace shopsnackDC.Models.BUS
+{
+    public class shopsnackDCBUS
+    {
+        public static IEnumerable<SanPham> DanhSach()
+        {
+            var db = new shopsnackDCConnectionDB();
+            return db.Query<SanPham>("select * from SanPham where TinhTrang = 1");
+        }
+        public static SanPham ChiTietSP(String a)
+        {
+            var db = new shopsnackDCConnectionDB();
+            return db.SingleOrDefault<SanPham> ("select * from SanPham sp join NhaSanXuat nsx on nsx.MaNhaSX = sp.MaNhaSX where MaSP = @0" , a);
+        }
+
+    }
+} 
